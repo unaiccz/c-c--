6b6f8dc2844bcd2e91f445f50138f8ceb5ed17c6
unai <unaicc2003@gmail.com>
2023-10-17 13:39:42 +0200

    i0

---
+++ lib/aritmetica.h
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int sum(int x,int y){
+    return(x+y);
+};
+int resta(int x, int y){
+    return (x-y);
+};
+int multiplicacion(int x, int y){
+
+    return(x*y);
+};
+int division(int x, int y){
+
+    return(x/y);
+};
---
+++ main.c
@@ -0,0 +1,9 @@
+#include "lib/aritmetica.h"
+int main(){
+    printf(" el numero sumado es %d\n", sum(9,9));
+    printf(" el numero restado es %d\n", resta(9,9));
+    printf(" el numero multiplicado es %d\n", multiplicacion(9,9));
+    printf(" el numero dividido es %d\n", division(9,9));
+}
+
+#
